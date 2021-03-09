7488cef2ec8907bacb67ffc7e393e18a4cf92b29
Tallys <tallys.prado@gmail.com>
2021-03-09 20:15:24 -0300

    image and anim

--- pages/Landing/index.js
+++ pages/Landing/index.js
@@ -1,17 +1,29 @@
-import Typing from 'react-typing-animation'
+import Typing from "react-typing-animation";
 
 export default function Landing() {
-    
-    return (
-        <div id='landing'>
-            <h1>Olá mundo!</h1>
-            <div className='typewriter'>
-                <h1 id='text'>
-                    Me chamo Tallys, sou programador e estudante!
-                </h1>
-            </div>
-            
+  return (
+    <>
+    <div className='bg'></div>
+      <div id="landing">
+        <div className="bg" />
+        <div className="block">
+          <div className="hello">
+            <h1>OLÁ</h1>
+            <h1>MUNDO</h1>
+          </div>
+
+          <div>
+            <h1>Bem vindo</h1>
+            <h1>à minha página.</h1>
+          </div>
         </div>
-    )
-    
+        <div className="typewriter">
+          <h1 id="text">
+            Meu nome é Tallys.
+            <br />
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
